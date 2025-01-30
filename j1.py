5d455667666792d4e248567f6b873032c5730cbf
Navy Sam <navyantu@example.com>
2025-01-29 17:16:49 -0800

    make j1 more informative by printing the devices info.

--- j1.py
+++ j1.py
@@ -1,17 +1,22 @@
 import ray
 import jax
+import pprint 
 
 # To submit the job to Ray:
 # ray job submit --runtime-env-json='{"working_dir": "."}' -- python j1.py
+# Or run it directly at the head node.
 
 ray.init()
 
 @ray.remote(resources={"TPU": 4})
 def my_function() -> int:
-  return jax.device_count()
+  return [str(x) for x in (jax.devices(), jax.device_count(), jax.local_devices(), jax.process_indices(), jax.process_index(), jax.process_count())]
 
 
 num_tpus = ray.available_resources()["TPU"]
+print("number of tpus: %d" % num_tpus)
 num_hosts = int(num_tpus) // 4
 h = [my_function.remote() for _ in range(num_hosts)]
-print(ray.get(h)) # [16, 16, 16, 16]
+
+print("jax.devices(), jax.device_count(), jax.local_devices(), jax.process_indices(), jax.process_index(), jax.process_count()")
+pprint.pp(ray.get(h)) # [16, 16, 16, 16]
