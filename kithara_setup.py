7a86a3f579e62fc8ce838d844eee99657e0c645a
root <yixinshi@example.com>
2025-02-07 16:43:23 -0800

    Clean up the installation code

--- kithara_setup.py
+++ kithara_setup.py
@@ -9,7 +9,25 @@ print(resources)
 num_nodes = len(ray.nodes())
 print(f"Number of nodes in the cluster: {num_nodes}")
 
+# Example usage
+cmd = """rm -rf /tmp/test* &&
+         mkdir -p /tmp/test && cd /tmp/test &&
+         git clone --recursive https://github.com/wenxindongwork/keras-tuner-alpha.git &&
+         cd keras-tuner-alpha &&
+         git checkout feb5_snapshot &&
+         pip install -r requirements.txt && 
+         pip install libtpu-nightly==0.1.dev20241010+nightly -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+      """ 
+#pip install -e ./maxtext --no-deps &&
+# Submit the task to run remotely on the cluster (head node or workers)
+
+num_tpus = ray.available_resources()["TPU"]
+num_hosts = int(num_tpus) // 4
+print("number of tpus: {} and hosts: {}".format(num_tpus, num_hosts))
+
+
 # Define the remote function to install a Python package
+# @ray.remote(resources={"TPU": 4})
 @ray.remote(resources={"TPU": 4})
 def install_dependencies(cmd):
     try:
@@ -25,17 +43,7 @@ def install_dependencies(cmd):
     except Exception as e:
         return f"Exception: {str(e)}"
 
-# Example usage
-cmd = """rm -rf /tmp/test* &&
-         mkdir -p /tmp/test && cd /tmp/test &&
-         git clone --recursive https://github.com/wenxindongwork/keras-tuner-alpha.git &&
-         cd keras-tuner-alpha &&
-         pip install -r requirements.txt && 
-         pip install -e ./maxtext --no-deps &&
-         pip install libtpu-nightly==0.1.dev20241010+nightly -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-      """ 
-# Submit the task to run remotely on the cluster (head node or workers)
-future = [install_dependencies.remote(cmd) for _ in range(4)]
+future = [install_dependencies.remote(cmd) for _ in range(num_hosts)]
 
 # Wait for the result
 result = ray.get(future)
